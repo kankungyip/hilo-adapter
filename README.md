08326c3935d625f9188560a28fc77aa52467c775
Yeqin Gong <gon@gyeq.in>
2018-04-09 15:25:30 +0800

    exports more classes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Hilo for minigame
+Hilojs for minigame
 ===
 
--- index.js
+++ index.js
@@ -17,11 +17,18 @@ var ImageLoader = require('hilojs/loader/ImageLoader')
 var ScriptLoader = require('hilojs/loader/ScriptLoader')
 var LoadQueue = require('hilojs/loader/LoadQueue')
 
+var HTMLAudio = require('hilojs/media/HTMLAudio')
+var WebAudio = require('hilojs/media/WebAudio')
 var WebSound = require('hilojs/media/WebSound')
+
+var Renderer = require('hilojs/renderer/Renderer')
+var CanvasRenderer = require('hilojs/renderer/CanvasRenderer')
+var WebGLRenderer = require('hilojs/renderer/WebGLRenderer')
 
 var Ease = require('hilojs/tween/Ease')
 var Tween = require('hilojs/tween/Tween')
 
+var drag = require('hilojs/util/drag')
 var TextureAtlas = require('hilojs/util/TextureAtlas')
 var Ticker = require('hilojs/util/Ticker')
 
@@ -39,6 +46,7 @@ var View = require('hilojs/view/View')
 
 module.exports['__esModule'] = true
 module.exports.default = Hilo
+module.exports.Class = Class
 module.exports.Mixin = Mixin
 
 // fixed: Hilo.getElementRect
@@ -49,8 +57,9 @@ if (!window.getComputedStyle) {
 }
 
 // Mixins
-Hilo.CacheMixin = CacheMixin
 Hilo.EventMixin = EventMixin
+Hilo.DraggableMixin = drag
+Hilo.CacheMixin = CacheMixin
 
 // Static Objects
 Hilo.WebSound = WebSound
@@ -64,6 +73,11 @@ Hilo.Matrix = Matrix
 Hilo.ImageLoader = ImageLoader
 Hilo.ScriptLoader = ScriptLoader
 Hilo.LoadQueue = LoadQueue
+Hilo.HTMLAudio = HTMLAudio
+Hilo.WebAudio = WebAudio
+Hilo.Renderer = Renderer
+Hilo.CanvasRenderer = CanvasRenderer
+Hilo.WebGLRenderer = WebGLRenderer
 Hilo.Tween = Tween
 Hilo.TextureAtlas = TextureAtlas
 Hilo.Ticker = Ticker
@@ -81,8 +95,21 @@ Hilo.Stage = Class.create({
   Extends: Stage,
   constructor: function (properties) {
     properties = properties || {}
+    var { width, height } = canvas
+    var scale = 1
+    if (properties.retina) {
+      scale = 0.5
+      width *= 2
+      height *= 2
+    }
     properties.canvas = canvas
+    properties.width = width
+    properties.height = height
+    properties.scaleX = scale
+    properties.scaleY = scale
     Hilo.Stage.superclass.constructor.call(this, properties)
+
+    this.scale = scale
 
     // fixed: Stage.updateViewport
     canvas.parentNode = document.body
